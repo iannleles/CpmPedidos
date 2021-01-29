9e553151842eecabf2e051da70af4b0d5b71c67f
Iann Leles <iannleles@outlook.com>
2021-01-28 21:57:24 -0300

    Injecao de dependencia com .Net 5

---
+++ CpmPedidos.API/Controllers/AppBaseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CpmPedidos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AppBaseController : ControllerBase
+    {
+        protected readonly IServiceProvider ServiceProvider;
+
+        public AppBaseController(IServiceProvider serviceProvider)
+        {
+            ServiceProvider = serviceProvider;
+        }
+        
+    }
+}
