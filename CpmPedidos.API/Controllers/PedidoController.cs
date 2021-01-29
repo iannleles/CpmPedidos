1c9d92379a892a55925607de8fc6cceeff1d603d
Iann Leles <iannleles@outlook.com>
2021-01-28 21:24:25 -0300

    Alterando controllers

---
+++ CpmPedidos.API/Controllers/PedidoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CpmPedidos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PedidoController : ControllerBase
+    {           
+
+        public PedidoController()
+        {
+            
+        }
+        
+    }
+}
